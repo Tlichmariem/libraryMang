1ea242aaa0abd9cc9269333598314622c45e2800
Tlichmariem <tlichmariem@gmail.com>
2024-12-17 22:06:07 +0100

    Mise à jour des fichiers

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,16 +1,49 @@
 <?php
-
+// src/DataFixtures/AppFixtures.php
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        // Ajouter 10 utilisateurs
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setRoles(['ROLE_USER']); // Roles de base
+            $user->setIsVerified($faker->boolean()); // Valeur aléatoire pour 'is_verified'
+
+            // Hash du mot de passe
+            $user->setPassword($this->passwordHasher->hashPassword($user, 'password123'));
+
+            $manager->persist($user);
+        }
+
+        // Ajouter un utilisateur administrateur
+        $admin = new User();
+        $admin->setEmail('admin@example.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setIsVerified(true); // L'administrateur est toujours vérifié
+
+        // Hash du mot de passe
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'admin123'));
+
+        $manager->persist($admin);
 
         $manager->flush();
     }
